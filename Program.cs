27a6238704362581a685b00812c4077d1e8c6013
inaciarodrigues <soterinacia688@gmail.com>
2025-03-31 12:00:45 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string PalavraSecreta = "";
+
+Console.Write("Palavra Secreta: ");
+PalavraSecreta = Console.ReadLine()!;
+
+Console.Write("Letra: ");
+char Letra = char.Parse(Console.ReadLine()!.Trim().Substring(0,1).ToLower());
+
+Console.WriteLine($"A letra {Letra} existe na palavra secreta? => {PalavraSecreta.Contains(Letra)}");
